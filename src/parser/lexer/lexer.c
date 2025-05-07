96b29e6fb7bf1b1738beee9fe2586ef1377e6e42
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-05-07 09:06:17 +0200

    Refactorizar lexer
    Llamar a la primera variable que toma la función como line en vez de command ya que procesa la entrada completa del usuario no solo un comando

--- src/parser/lexer/lexer.c
+++ src/parser/lexer/lexer.c
@@ -6,13 +6,33 @@
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 11:30:39 by jalcausa          #+#    #+#             */
-/*   Updated: 2025/05/06 11:34:16 by jalcausa         ###   ########.fr       */
+/*   Updated: 2025/05/07 09:03:37 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_list	*lexer(char *command, t_shell_data *data)
+t_lex_st	lex_next_state(t_lex_st state, char line)
+{
+	// Si estamos entre comillas simples o dobles y se cierran
+	// volvemos al estado LEX_WORD, si estamos en el estado inicial
+	// y el símbolo actual no es ninguno de los especiales pasamos a LEX_WORD
+	if ((state == LEX_SIMPLE_QUOTE && line == '\'')
+		|| (state == LEX_DOUBLE_QUOTE && line == '"')
+		|| (state == LEX_START && !ft_strchr("<' |\">", line)))
+		return (LEX_WORD);
+	else if ((state == LEX_START && line == '\'')
+		|| (state == LEX_WORD && line == '\''))
+		return (LEX_SIMPLE_QUOTE);
+	else if ((state == LEX_START && line == '"')
+		|| (state == LEX_WORD && line == '"'))
+		return (LEX_DOUBLE_QUOTE);
+	else if ((state == LEX_WORD && ft_strchr("< |>", line)))
+		return (LEX_START);
+	return (state);
+}
+
+t_list	*lexer(char *line, t_shell_data *data)
 {
 	t_lex_st	state;
 	t_list		*tokens;
@@ -20,18 +40,18 @@ t_list	*lexer(char *command, t_shell_data *data)
 	(void) data;
 	state = LEX_START;
 	tokens = 0;
-	if (!command)
+	if (!line)
 		return (0);
-	while (*command)
+	while (*line)
 	{
 		if (state == LEX_START)
-			lex_start_state(&command, &state, &tokens);
+			lex_start_state(&line, &state, &tokens);
 		else if (state == LEX_SIMPLE_QUOTE)
-			lex_quote_states(&command, &state, &tokens, '\'');
+			lex_quote_states(&line, &state, &tokens, '\'');
 		else if (state == LEX_DOUBLE_QUOTE)
-			lex_quote_states(&command, &state, &tokens, '"');
+			lex_quote_states(&line, &state, &tokens, '"');
 		else if (state == LEX_WORD)
-			lex_word_state(&command, &state, &tokens);
+			lex_word_state(&line, &state, &tokens);
 	}
 	if (state == LEX_SIMPLE_QUOTE || state == LEX_DOUBLE_QUOTE)
 	{
