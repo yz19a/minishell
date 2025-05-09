54b5981995aab4c946c16761f516519e6a7641ad
yanel <yanelperaltarodriguez77@gmail.com>
2025-05-09 15:55:51 +0200

    subir cambios repositorio local a remoto

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,65 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:07:21 by jalcausa          #+#    #+#             */
-/*   Updated: 2025/04/15 22:45:08 by jalcausa         ###   ########.fr       */
+/*   Updated: 2025/05/05 20:16:49 by yaperalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHELL_H
-# include "../libft/libft.h"
-# include "lexer.h"
-# include "parser.h"
-# include <unistd.h>
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <errno.h>
-# include <string.h>
-# include <fcntl.h>
-# include <signal.h>
-# include <stdlib.h>
+#define MINISHELL_H
+#include "../libft/libft.h"
+#include "lexer.h"
+#include "parser.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
 
 // Estructuras:
 
 typedef struct s_command
 {
-	char	**argv;
-	int		argc;
-	int		fd_in;
-	int		fd_out;
-}	t_command;
+	char **argv;
+	int argc;
+	int fd_in;
+	int fd_out;
+} t_command;
 
 typedef struct s_shell_data
 {
-	char		**env;
-	int			dup_stdin;
-	int			dup_stdout;
-	t_list		*commands;
-	char		**exportenv;
-}	t_shell_data;
+	char **env;
+	int dup_stdin;
+	int dup_stdout;
+	t_list *commands;
+	char **exportenv;
+} t_shell_data;
 
 typedef struct s_global_sig
 {
-	int		exit_status;
-	pid_t	pid;
-}	t_global_sig;
+	int exit_status;
+	pid_t pid;
+} t_global_sig;
 
 // Prototipos de funciones:
 
 // Inicializar datos:
 
-t_shell_data	*init_shell_data(char **env);
-void			sig_init(void);
+t_shell_data *init_shell_data(char **env);
+void sig_init(void);
 
-// Manejo de señales
-void	set_signals_interactive(void);
-void	ignore_sigquit(void);
-void	signal_reset_prompt(int signal);
+// Manejo de señales:
+void set_signals_interactive(void);
+void ignore_sigquit(void);
+void signal_reset_prompt(int signal);
 
-extern t_global_sig	g_sig;
+extern t_global_sig g_sig;
+
+// Executer:
 
 #endif
---
+++ src/executer/exec_export.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_export.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 16:36:47 by yaperalt          #+#    #+#             */
+/*   Updated: 2025/04/19 19:21:15 by yaperalt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/**
+ * Implements the export command to set/display environment variables.
+ */
