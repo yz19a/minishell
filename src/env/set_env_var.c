e61573ae963e3066ab54f4fc33928d888dc0456e
yanel <yanelperaltarodriguez77@gmail.com>
2025-05-25 16:06:50 +0200

    updated functions

--- src/env/set_env_var.c
+++ src/env/set_env_var.c
@@ -6,7 +6,7 @@
 /*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 10:27:06 by yaperalt          #+#    #+#             */
-/*   Updated: 2025/05/25 10:39:35 by yaperalt         ###   ########.fr       */
+/*   Updated: 2025/05/25 16:06:30 by yaperalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,52 @@ int	set_env_var(t_shell_data *data, char *name, char *value)
 		data->env[idx] = ft_strjoin(name, value);
 	}
 	free(aux);
+	return (0);
+}
+
+static char	**realloc_export_env_vars(t_shell_data *data, int size)
+{
+	char	**new_env;
+	int		i;
+
+	new_env = ft_calloc(size + 1, sizeof * new_env);
+	if (!new_env)
+		return (0);
+	i = 0;
+	while (data->exportenv[i] && i < size)
+	{
+		new_env[i] = ft_strdup(data->exportenv[i]);
+		free_ptr(data->exportenv[i]);
+		i++;
+	}
+	free(data->exportenv);
+	return (new_env);
+}
+
+int	set_export_env_var(t_shell_data *data, char *key, char *value)
+{
+	int		idx;
+	char	*aux;
+
+	idx = get_env_var_index(data->exportenv, key);
+	if (!value)
+		value = "";
+	aux = ft_strjoin("=", value);
+	if (!aux)
+		return (12);
+	if (idx != -1 && data->exportenv[idx])
+	{
+		free_ptr(data->exportenv[idx]);
+		data->exportenv[idx] = ft_strjoin(key, aux);
+	}
+	else
+	{
+		idx = env_size(data->exportenv);
+		data->exportenv = realloc_export_env_vars(data, idx + 1);
+		if (!data->exportenv)
+			return (12);
+		data->exportenv[idx] = ft_strjoin(key, aux);
+	}
+	free_ptr(aux);
 	return (0);
 }
