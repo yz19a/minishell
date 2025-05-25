ffeaa2c3acbdf2981c609ece1d1185697d198377
yanel <yanelperaltarodriguez77@gmail.com>
2025-05-25 15:32:57 +0200

    updated .h with function prototypes

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:07:21 by jalcausa          #+#    #+#             */
-/*   Updated: 2025/05/25 15:25:29 by jalcausa         ###   ########.fr       */
+/*   Updated: 2025/05/25 15:34:16 by yaperalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,119 +31,152 @@
 
 typedef struct s_command
 {
-	char **argv;
-	int argc;
-	int fd_in;
-	int fd_out;
+    char **argv;
+    int argc;
+    int fd_in;
+    int fd_out;
 } t_command;
 
 typedef struct s_shell_data
 {
-	char **env;
-	int dup_stdin;
-	int dup_stdout;
-	t_list *commands;
-	char **exportenv;
+    char **env;
+    int dup_stdin;
+    int dup_stdout;
+    t_list *commands;
+    char **exportenv;
 } t_shell_data;
 
 typedef struct s_global_sig
 {
-	int exit_status;
-	pid_t pid;
+    int exit_status;
+    pid_t pid;
 } t_global_sig;
 
 // Prototipos de funciones:
 
-// Inicializar datos (init.c):
+// init.c
+t_shell_data	*init_shell_data(char **env);
+void			sig_init(void);
 
-t_shell_data *init_shell_data(char **env);
-void sig_init(void);
-
-// Manejo de señales:
-void set_signals_interactive(void);
-void ignore_sigquit(void);
-void signal_reset_prompt(int signal);
-
-// Manejo de señales (signals.c)
+// signals.c
 void	set_signals_interactive(void);
 void	ignore_sigquit(void);
 void	signal_reset_prompt(int signal);
 void	print_newline(int signal);
 void	set_signals_noninteractive(void);
 
-// Bucle principal (shell_loop.c)
+// shell_loop.c
 void	shell_loop(t_shell_data *data);
 
 // free.c
 void	free_ptr(void *ptr);
 void	free_envs(t_shell_data *data);
 void	del_t_command(void *com);
+void	free_command(t_command *cmd);
+void	free_exportenv(char **exportenv);
 
-//exit.c
+// exit.c
 void	exit_shell(t_shell_data *data, int exitcode);
 
 // expand_variables.c
 void	expand_variables(char **line, t_shell_data *data, int expand_quotes);
+char	*expand_env_var(char *var, t_shell_data *data);
 
-// env
+// env.c
 char	*get_env_value(t_shell_data *data, char *key);
 char	**split_env(char *str);
+int		set_env_var(t_shell_data *data, char *name, char *value);
+int		unset_env_var(t_shell_data *data, char *name);
+int		add_env_var(t_shell_data *data, char *name, char *value);
+void	print_env(char **env);
 
-extern t_global_sig g_sig;
 // errors.c
 char	*join_strs(char *str, char *add);
-int	print_error(char *command, char *detail, char *error_message, int err);
+int		print_error(char *command, char *detail, char *error_message, int err);
 
-// LEXER
 // lexer.c
 t_list	*lexer(char *command, t_shell_data *data);
 t_lex_st	lex_next_state(t_lex_st state, char cur_char);
 
-// states.c
+// lexer/states.c
 void	lex_quote_states(char **line, t_lex_st *st, t_list **res, char quote);
 void	lex_word_state(char **line, t_lex_st *st, t_list **res);
 void	lex_start_state(char **line, t_lex_st *state, t_list **res);
 
-// utils.c
+// lexer/utils.c
 t_list	*lex_create_token(t_token_type type, char *content);
 void	append_last_token(t_list **tokens, char **cmd);
 void	lex_free_token_list(t_list **lst);
 
-// PARSER
 // parser.c
-t_list	*parser(t_list *tokens, t_shell_data *data);
+t_list		*parser(t_list *tokens, t_shell_data *data);
 t_pars_st	pars_next_state(t_pars_st st, t_token *tok);
 
-//states.c
-t_pars_err	pars_start_st(t_list *tokens, t_pars_st *state,
-	t_list **commands);
-t_pars_err	pars_command_st(t_list *tokens, t_pars_st *state,
-	t_list **commands);
-t_pars_err	pars_redirin_st(t_list *tokens, t_pars_st *state,
-	t_list **commands);
-t_pars_err	pars_redirout_st(t_list *tokens, t_pars_st *state,
-	t_list **cmds);
-t_pars_err	pars_rediroappe_st(t_list *tokens, t_pars_st *state,
-	t_list **cmds);
+// parser/states.c
+t_pars_err	pars_start_st(t_list *tokens, t_pars_st *state, t_list **commands);
+t_pars_err	pars_command_st(t_list *tokens, t_pars_st *state, t_list **commands);
+t_pars_err	pars_redirin_st(t_list *tokens, t_pars_st *state, t_list **commands);
+t_pars_err	pars_redirout_st(t_list *tokens, t_pars_st *state, t_list **cmds);
+t_pars_err	pars_rediroappe_st(t_list *tokens, t_pars_st *state, t_list **cmds);
 
-// states2.c
-t_pars_err	pars_redheredoc_st(t_list *tokens, t_pars_st *state,t_list **cmds,
-	t_shell_data *data);
+// parser/states2.c
+t_pars_err	pars_redheredoc_st(t_list *tokens, t_pars_st *state, t_list **cmds, t_shell_data *data);
 t_pars_err	pars_invalid_st(t_list *tokens, t_pars_st *state);
 
-// actions.c
+// parser/actions.c
 t_pars_err	pars_set_stdin(t_list *commands, char *filename);
 t_pars_err	pars_set_stdout(t_list *commands, char *filename, int append);
 t_pars_err	pars_append_arg_to_command(t_list *commands, char *value);
 t_pars_err	pars_append_new_command(t_list **commands, char *value);
 
-// actions_heredoc.h
-t_pars_err	pars_create_heredoc(t_list	*cmds, char *value, t_shell_data *data);
+// parser/actions_heredoc.c
+t_pars_err	pars_create_heredoc(t_list *cmds, char *value, t_shell_data *data);
 
-// utils.c
+// parser/utils.c
 void	pars_free_command_list(t_list **cmds);
 
+// Executer
+// execute.c
+char	*check_access(char *command, char **path);
+int		execute(t_list *instr, t_shell_data *data);
+int		execute_pipex(t_shell_data *data);
 
+// execute_utils.c
+int		decode_error(int err);
+int		is_builtin(char *command);
+int		execute_builtins(t_command *command, t_shell_data *data);
+
+// parse_env.c
+char	**get_path(char **env);
+void	free_path(char **path);
+
+// exec_cd.c
+int		built_in_cd(t_command *command, t_shell_data *data);
+
+// exec_echo.c
+int		built_in_echo(t_command *command);
+
+// exec_env.c
+int		built_in_env(t_command *command, t_shell_data *data);
+
+// exec_exit.c
+void	exitshell(t_shell_data *data, int exitcode);
+int		built_in_exit(t_command *command, t_shell_data *data);
+
+// exec_export.c
+int		built_in_export(t_command *command, t_shell_data *data);
+
+// exec_pwd.c
+int		exec_pwd(void);
+
+// exec_unset.c
+int		built_in_unset(t_command *command, t_shell_data *data);
+
+// utils.c (general)
+void	print_prompt(void);
+void	clear_input_buffer(void);
+
+// global signal
 extern t_global_sig	g_sig;
 
 // Executer:
@@ -173,8 +206,5 @@ int built_in_export(t_command *command, t_shell_data *data);
 int exec_pwd(void);
 /* exec_unset.c */
 int built_in_unset(t_command *command, t_shell_data *data);
-/* utils/pipes.c */
-void	close_pipes(t_shell_data *data, t_list *instr);
-
 
 #endif
